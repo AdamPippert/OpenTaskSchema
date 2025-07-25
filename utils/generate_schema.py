5bc1201ef2871502bc2c1102be15951eba0caaea
Anton Abashkin <anton@appsec.science>
2025-07-25 17:02:53 -0600

    add schema metadata

--- utils/generate_schema.py
+++ utils/generate_schema.py
@@ -3,5 +3,10 @@ import json
 
 schema = JothTask.model_json_schema()
 
-with open('joth_task_schema.json', 'w') as f:
+schema["$schema"] = "https://json-schema.org/draft/2020-12/schema"
+schema["$id"] = "https://github.com/AdamPippert/JOTH"
+schema["title"] = "JOTH Task Schema"
+schema["version"] = "0.0.1"
+
+with open('joth_schema.json', 'w') as f:
     json.dump(schema, f, indent=2)
